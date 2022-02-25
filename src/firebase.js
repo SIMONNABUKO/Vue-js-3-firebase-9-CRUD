898921ae877926412adf5e9ccd6b4bfe7df2f889
Simon Angatia <simonnabuko@gmail.com>
2022-02-25 14:08:15 +0300

    finished CRUD

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore";
 const firebaseConfig = {
+  //USE YOUR CREDENTIALS HERE, i AM GOING TO DELETE THESE
   apiKey: "AIzaSyD_KYCrBaNwm1VFxGv9wXZCO5eOOkP2hUY",
   authDomain: "vue3firebase9.firebaseapp.com",
   projectId: "vue3firebase9",
